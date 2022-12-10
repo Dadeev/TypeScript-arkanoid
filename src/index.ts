b177d6e7f81389708c28eabd8f0f8df29fd3ed2e
Tima <dadeev.tim@gmail.com>
2022-12-10 16:27:01 +0700

    feat: add CanvaView class and start implamenting functionalaty into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-// Start here
-console.log("Start here!");
+import { CanvasView } from './view/CanvasView';
+import { Ball } from './sprites/Ball';
+import { Brick } from './sprites/Brick';
+import { Paddle } from './sprites/Paddle';
+import PADDLE_IMAGE from './images/paddle.png';
+import BALL_IMAGE from './images/ball.png';
+import {
+    PADDLE_SPEED,
+    PADDLE_WIDTH,
+    PADDLE_HEIGHT,
+    PADDLE_STARTX,
+    BALL_SPEED,
+    BALL_SIZE,
+    BALL_STARTX,
+    BALL_STARTY
+} from './setup';
+
+let score = 0;
+let gameOver = false;
+
+function setGameOver(view: CanvasView) {
+    view.drawInfo('Game Over!')
+    gameOver = false;
+}
+
+function setGameWin(view: CanvasView) {
+    view.drawInfo('Game Won!')
+    gameOver = false;
+}
